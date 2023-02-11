7d8d8d97b621cc86e13284126034263db81eb6c9
Pedro <contatopdrm@gmail.com>
2023-02-11 01:35:54 -0300

    exercise2 done

--- ATVSP2/tutoria2/exercicio2/main.c
+++ ATVSP2/tutoria2/exercicio2/main.c
@@ -2,5 +2,18 @@
 #include <stdio.h>
 
 int main(){
+    int n;
+    scanf("%d",&n);
 
+    while(n != 0){
+        if(soma_r9(n) % 9 == 0){
+            printf("%d is a multiple of 9\n",n);
+        }else{
+            printf("%d is not a multiple of 9\n",n);
+        }
+
+        scanf("%d",&n);
+    }
+
+    return 0;
 }
--- ATVSP2/tutoria2/exercicio2/r9.c
+++ ATVSP2/tutoria2/exercicio2/r9.c
@@ -1,5 +1,9 @@
 #include "r9.h"
 
 int soma_r9(int n){
-    
+    int digito = n % 10;
+    if(n > 0){
+        return digito + soma_r9(n / 10);
+    }
+    return n;   
 }
