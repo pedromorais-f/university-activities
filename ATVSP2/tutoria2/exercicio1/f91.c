7e02a69d761e36dfb76c07a0e24b55b990f5813d
Pedro <contatopdrm@gmail.com>
2023-02-11 00:52:11 -0300

    exercise1 done

--- ATVSP2/tutoria2/exercicio1/f91.c
+++ ATVSP2/tutoria2/exercicio1/f91.c
@@ -1,5 +1,9 @@
 #include "f91.h"
 
 int f91 (int n){
-    
+    if(n >= 101){
+        return n - 10;
+    }else{
+        return f91(f91(n + 11));
+    }
 }
--- ATVSP2/tutoria2/exercicio1/main.c
+++ ATVSP2/tutoria2/exercicio1/main.c
@@ -2,5 +2,14 @@
 #include "f91.h"
 
 int main (){
-    
+    int n,resultado;
+    scanf("%d",&n);
+
+    while(n != 0){
+        resultado = f91(n);
+        printf("f91(%d) = %d\n",n,resultado);
+        scanf("%d",&n);
+    }
+
+    return 0;
 }
