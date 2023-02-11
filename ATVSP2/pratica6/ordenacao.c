ee98b82cd51f3931ed26c8d07341a67a915272c0
Pedro <contatopdrm@gmail.com>
2023-02-11 00:28:08 -0300

    pratica6 done

--- ATVSP2/pratica6/ordenacao.c
+++ ATVSP2/pratica6/ordenacao.c
@@ -4,11 +4,26 @@
 
 //Manter como especificado
 int *alocaVetor(int *vetor, int n){
+    int *v;
+    v = malloc(n * sizeof(int));
+    return v;
 }
 
 //Manter como especificado
 int *desalocaVetor(int *vetor){
+    free(vetor);
+    return vetor;
 }
 
 void ordenacao(int *vetor, int n, int *movimentos){
+    int aux;
+    int j;
+    for(int i = 1;i < n;i++){
+        aux = vetor[i];
+        for(j = i - 1;j >= 0 && aux < vetor[j];j--){
+            vetor[j + 1] = vetor[j];
+            *movimentos += 1;
+        }
+        vetor[j + 1] = aux;
+    }
 }
--- ATVSP2/pratica6/pratica.c
+++ ATVSP2/pratica6/pratica.c
@@ -9,10 +9,22 @@ int main ()
 	while(scanf("%d", &n), n)
 	{
 		//alocar o vetor
+		vetor = alocaVetor(vetor,n);
 		//preencher o vetor
+		for(int i = 0;i < n;i++){
+			scanf("%d",&vetor[i]);
+		}
 		//ordenar o vetor e determinar o número de movimentos
+		ordenacao(vetor,n,&movimentos);
 		//imprimir o resultado
+		if(movimentos % 2 == 0){
+			printf("Carlos\n");
+		}else{
+			printf("Marcelo\n");
+		}
 		//desalocar o vetor
+		vetor = desalocaVetor(vetor);
+		movimentos = 0;
 	}
 
 	return 0;
