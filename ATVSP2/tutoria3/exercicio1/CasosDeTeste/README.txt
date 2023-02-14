84efcbba7b1d87a62198d48c7cf4b21ef7f6f23b
Pedro <contatopdrm@gmail.com>
2023-02-13 21:06:15 -0300

    add files tutoria3

---
+++ ATVSP2/tutoria3/exercicio1/CasosDeTeste/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 13/02/2023 17:26:46 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Semana 3 - Prática
---
+++ ATVSP2/tutoria3/exercicio1/ordenacao.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ordenacao.h"
+
+struct upa{
+
+};
+
+int alocarUpa (TADupa **aux,int n){
+    
+}
+
+void preencheVetor(TADupa *upas, int n){
+    
+}
+
+void ordenaUpas(TADupa *upas, int n){
+    
+}
+
+void imprimeUpas(TADupa *upas, int n){
+    
+}
+
+void desalocaUpas(TADupa **aux){
+    
+}
