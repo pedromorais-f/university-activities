6d1a818af936364f691dae5ec2f011e87c396704
Pedro <contatopdrm@gmail.com>
2023-02-17 23:01:50 -0300

    pratica7 almost done

--- ATVSP2/pratica7/ordenacao.c
+++ ATVSP2/pratica7/ordenacao.c
@@ -6,35 +6,81 @@
 // Manter como especificado
 void ordena(Aluno *alunos, int n) {
 	// PREENCHER AQUI
+    int i,j;
+    Aluno aux;
+    int espaco = 1;
+    
+    do{
+        espaco = 3 * espaco + 1;
+    }while(espaco < n);
+
+    do{
+        espaco /= 3;
+        for(i = espaco;i < n;i++){
+            aux = alunos[i];
+            j = i - espaco;
+
+            while(j >= 0 && compare(aux.nome,alunos[j].nome) < 0){
+                alunos[j + espaco] = alunos[j];
+                j -= espaco;
+            }
+            alunos[j + espaco] = aux;
+        }
+    }while(espaco > 1);
+    
+
+    /*for(int i = 0;i < n;i++){
+        printf("%s\n",alunos[i].nome);
+    }*/
 }
 
 // Manter como especificado
 int compare(const char* aluno1, const char* aluno2) {
     // PREENCHER AQUI
+    int comparacao;
+    comparacao = strcmp(aluno1,aluno2);
+    return comparacao;
 }
 
 /* Manter como especificado */
 void imprimeResposta(Aluno* alunos, int n, int k) {
-    // PREENCHER AQUI 
+    // PREENCHER AQUI
+    for(int i = 0;i < n;i++){
+        if(i == k - 1){
+            printf("%s\n",alunos[i].nome);
+            break;
+        }
+    }
 }
 
 /* Manter como especificado */
 Aluno* alocaAlunos(int n) {
     // PREENCHER AQUI 
+    Aluno *alunos;
+    alunos = (Aluno*)malloc(n * sizeof(Aluno));
+    return alunos;
 }
 
 /* Manter como especificado */
 void liberaAlunos(Aluno** alunos) {
     // PREENCHER AQUI
+    free(*alunos);
 }
 
 /* Manter como especificado */
 void leAlunos(Aluno* alunos, int n) {
     // PREENCHER AQUI
+    for(int i = 0;i < n;i++){
+        scanf("%s",alunos[i].nome);
+    }
 }
 
 /* Manter como especificado */
 void leQuantidades(int* n, int* k) {
     // PREENCHER AQUI
+    scanf("%d",n);
+    getchar();
+    scanf("%d",k);
+    getchar();
 }
 
--- ATVSP2/pratica7/pratica.c
+++ ATVSP2/pratica7/pratica.c
@@ -8,21 +8,27 @@ int main ()
 
 	// ler as quantidades
 	// PREENCHER AQUI
+	leQuantidades(&n,&k);
 
 	// alocar o vetor dinamico de alunos
 	// PREENCHER AQUI
+	alunos = alocaAlunos(n);
 
 	// preencher o vetor
 	// PREENCHER AQUI
+	leAlunos(alunos,n);
 
 	// ordenar o vetor usando o shellsort
 	// PREENCHER AQUI
+	ordena(alunos,n);
 
 	// imprimir a resposta
 	// PREENCHER AQUI
+	imprimeResposta(alunos,n,k);
 
 	// desalocar o vetor
 	// PREENCHER AQUI
+	liberaAlunos(&alunos);
 
   	return 0;
 }
