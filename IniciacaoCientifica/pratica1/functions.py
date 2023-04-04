5c1b73d149bd6c7c8fb6968d3024eb7fb6a12d9c
Pedro <contatopdrm@gmail.com>
2023-04-04 16:26:29 -0300

    write and read functions of csv file done

--- IniciacaoCientifica/pratica1/functions.py
+++ IniciacaoCientifica/pratica1/functions.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import csv
 
 def file_path(extension):
@@ -13,7 +13,6 @@ def file_path(extension):
 
 
 def write_file():
-
     name_file = file_path("txt")
     f = open(name_file, "w")
 
@@ -45,18 +44,75 @@ def read_file():
 
     try:
         f = open(name_file, "r")
+        file_lines = f.readlines()
+        
+        for i, file_line in enumerate(file_lines):
+            file_line = file_line.replace("\n", "")
+            print(f"{i + 1}| {file_line}")
     except:
         print("\nError: The file could not be read")
         return
 
-    file_lines = f.readlines()
-    for i, file_line in enumerate(file_lines):
-        file_line = file_line.replace("\n", "")
-        print(f"{i + 1}| {file_line}")
         
         
     f.close()
     
-f = write_file()
+    
+def write_file_csv():
+    name_file = file_path("csv")
+    
+    data_list = [["NAME","EMAIL","DATE OF BIRTH"]]
+    flag = True
+
+    while flag:
+        validation = input("\nDo you wanna add information to the file? (yes or no):")
+
+        if validation == "yes":
+            new_flag = True
+            
+            name = input("\nWrite the name (write \"done\" when you finished):")
+            email = input("Write the email:")
+            date_of_birth = input("Write the date of birth:")
+            
+            while new_flag:
+                data_list_itens = [name, email, date_of_birth]
+                data_list.append(data_list_itens)
+                
+                name = input("\nWrite the name (write \"done\" when you finished):")
+                if name == "done":
+                    break
+
+                email = input("Write the email:")
+                date_of_birth = input("Write the date of birth:")
+        elif validation == "no":
+            flag = False
+        else:
+            print("Please, put a valid answer\n")
+            
+            
+        with open(name_file, "w") as f:
+            data_writer = csv.writer(f)
+            for row in data_list:
+                data_writer.writerow(row)
+           
+            
+        f.close()
+        
+def read_file_csv():
+    name_file = file_path("csv")
+    
+    try:
+        #with open(name_file, "r") as f:
+            #f_csv = csv.reader(f)
+            #for row in f_csv:
+                #print(row)
+        #f.close()
+            
+            
+        f_csv = pd.read_csv(name_file)
+        print(f_csv)
+    except:
+        print("\nError: The file could not be read")
+        return
 
 
