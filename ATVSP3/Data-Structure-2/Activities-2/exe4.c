3af8f3ecc177d93f9dc786a9538d54860abbb764
Pedro <contatopdrm@gmail.com>
2023-06-17 23:12:16 -0300

    realloc Data Structure 2 dir

---
+++ ATVSP3/Data-Structure-2/Activities-2/exe4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define MM 4
+
+typedef long TipoChave;
+
+typedef struct TipoRegistro
+{
+    TipoChave Chave;
+} TipoRegistro;
+
+typedef struct TipoPagina *TipoApontador;
+
+typedef struct TipoPagina
+{
+    short n;
+    TipoRegistro r[MM];
+    TipoApontador p[MM + 1];
+} TipoPagina;
+
+void Minimo(TipoRegistro *x, TipoApontador Ap)
+{
+    int pag = 1;
+    if(Ap->p[pag - 1] != NULL)
+        return Minimo(x, Ap->p[pag - 1]);
+    else
+        *x = Ap->r[0];
+}
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
