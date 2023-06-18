e726d9369ad61df16ea5e0b89fb8c1c94d0b0051
Pedro <contatopdrm@gmail.com>
2023-06-18 02:36:08 -0300

    activity3 done

---
+++ ATVSP3/Functional-Programming/Activities-1/activity3.hs
@@ -0,0 +1,19 @@
+module Activity3 where
+
+bools :: [Bool]
+bools = [True, False, False, True]
+
+nums :: [[Int]]
+nums = [[1, 2], [3, 4, 5,  6,]]
+
+add :: Int -> Int -> Int -> Int
+add x y z = x + y + z
+
+copy :: a -> (a, a)
+copy x = (x , x)
+
+apply :: (a -> b) -> a -> b
+apply function x = y
+
+swap  :: (a,b) -> (b,a)
+swap (x ,y) = (y, x)
