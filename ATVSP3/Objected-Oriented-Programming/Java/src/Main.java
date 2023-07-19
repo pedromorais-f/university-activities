400df4efd9155e30703b1a49249ade926a9ddb54
Pedro <contatopdrm@gmail.com>
2023-07-18 22:05:00 -0300

    update JavaFile

--- ATVSP3/Objected-Oriented-Programming/Java/src/Main.java
+++
@@ -1,9 +0,0 @@
-import Exercises1.*;
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Static1.max3(1, 2, 3));
-        System.out.println(Static1.max3(1.0, 2.4, 3.1));
-    }
-}
