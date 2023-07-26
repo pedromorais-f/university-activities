f920afa7c8e89455a65d5c121c63ce9bda52297b
Pedro <contatopdrm@gmail.com>
2023-07-25 21:40:16 -0300

    fix activity13

--- ATVSP3/Functional-Programming/Activities-1/activity13.hs
+++ ATVSP3/Functional-Programming/Activities-1/activity13.hs
@@ -1,16 +1,9 @@
 module Activity where
 
 import Parser
-import Data.Char (isAscii)
 
-
-data Identifier = ID String deriving Show
-
-parserID :: Parser Char Identifier
-parserID = ID <$> greedy (sat isAscii) 
-
-idParser :: Parser Char [Identifier]
-idParser = listOf parserID (symbol ',')
+idParser :: Parser Char [String]
+idParser = listOf identifier (symbol ',')
 
 
 
